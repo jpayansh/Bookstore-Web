1ef26f313a9b21541a65ae09a644a100212a3c72
jpayansh <110517005+jpayansh@users.noreply.github.com>
2024-03-02 14:26:21 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5555;
-export const mongoDBURL = `mongodb+srv://root:root@book-store-mern.52sg7od.mongodb.net/?retryWrites=true&w=majority&appName=Book-store-MERN`;
+export const mongoDBURL = ``;
